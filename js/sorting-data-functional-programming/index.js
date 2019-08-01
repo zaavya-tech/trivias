b5707fb407d398a55ec7655c8e8d1540b7d1abe9
mohib_wasay_zt <mohib@zaavya.com>
2019-08-01 15:26:54 +0500

    sorting-data-functional-programming

---
+++ js/sorting-data-functional-programming/index.js
@@ -0,0 +1,15 @@
+/**
+ * Given: A list of users array of objects.
+ * @Requirement: Sort users by age and filter only female profiles and create a new list of users 
+  (not mutating the existing users list). You have to implement this using functional programming, also create a new array
+  of users without mutating the provided lists. 
+  
+*/
+
+const users = require('./users.json');
+
+function main() {
+  /* Your code goes here! */
+}
+
+main(users);
